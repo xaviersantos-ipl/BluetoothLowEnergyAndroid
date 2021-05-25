4060c08dfd9858b282fa0886f77df1c4783baed0
Xavier <2202615@my.ipleiria.pt>
2021-05-25 19:49:26 +0100

    Submit Status
    Submit is only enabled when a certain BLE device name is scanned

--- app/src/main/java/com/example/bluetoothlowenergytests/Constants.kt
+++ app/src/main/java/com/example/bluetoothlowenergytests/Constants.kt
@@ -2,7 +2,7 @@ package com.example.bluetoothlowenergytests
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://10.0.2.2:7579/"
+        const val BASE_URL = "http://bb045eb59571.ngrok.io/"
         const val USER = "superadmin"
         const val PASS = "f7c6c12d"
         const val REQUEST_TIMEOUT_DURATION = 10
